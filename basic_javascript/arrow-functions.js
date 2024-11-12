23448121014722ba948e2d103b263f9c927dc170
Kaium Al Limon <limon.3cs@gmail.com>
2024-11-12 20:30:24 +0600

    arrow functions add

---
+++ basic_javascript/arrow-functions.js
@@ -0,0 +1,25 @@
+// arrow functions
+
+// arrow function to add two numbers
+
+let add = (a,b) => a+b
+
+
+/*
+Arrow functions in javascript are concise syntax for writing functions,
+introduced in ES6. They are also known as lambda functions. They
+simplify function expressions and behave differently than the traditional
+functions in some ways.
+
+Usecases:
+1. Callbacks
+2. Event listeners
+3. Array methods
+4. Promises
+5. Higher order functions
+6. As object methods
+7. As constructors
+8. As generators
+9. As async functions
+10. As IIFE
+*/
