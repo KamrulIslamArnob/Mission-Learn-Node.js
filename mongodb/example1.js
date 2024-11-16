c4a301e46f944fe79fe524f4c08889baf5750efd
Kaium Al Limon <limon.3cs@gmail.com>
2024-11-16 15:14:00 +0600

    mongodb connection and a sample api create

--- mongodb/example1.js
+++ mongodb/example1.js
@@ -1,55 +1,9 @@
-// const {readFile, writeFile} = require('./node-core/filesystem');
-// const server = require('./node-core/http_module');
-// const filePath = require('./node-core/path_module');
-// const myUrl = require('./node-core/url_module');
-// // const expressServer = require('./express/express_framework');  
-// const mongoDBExample1 = require('./mongodb/example1');  
-// const dotenv = require('dotenv');
-
-
-// // load env variables
-// dotenv.config();
-
-// // get port from env or use 3000
-// // const port = process.env.PORT || 3000; 
-
-// // read file
-// // readFile('./node-core/example.txt');
-
-
-// // write to a file
-
-// // writeFile('./node-core/example.txt', 'Hello, Node.js!');
-
-
-// // read file after writing to it
-
-// // readFile('./node-core/example.txt');
-
-// // http module
-// // server.listen(3000,()=>{
-// //     console.log(`Server running on port http://localhost:3000`);
-// // })
-
-
-// // path module
-// // console.log(filePath);
-
-
-
-// // url module
-// // console.log(myUrl.href);
-// // console.log(myUrl.toString());
-// // console.log(myUrl.host);
-// // console.log(myUrl.hostname);
-// // console.log(myUrl.pathname);
-
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');  // Import bcrypt
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { password } = require('../node-core/url_module');
 
 // Create app
 const app = express();
@@ -75,6 +29,7 @@ mongoose.connect(dbUrl, {
     console.error('Error connecting to MongoDB: ', err);
 });
 
+
 // Create a schema for users
 const userSchema = new mongoose.Schema({
     name: String,
@@ -84,9 +39,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // Create a model for users
+
 const User = mongoose.model('User', userSchema);
 
 // Create a new user
+
 app.post('/users', async (req, res) => {
     const { name, age, email, password } = req.body;
 
@@ -96,7 +53,7 @@ app.post('/users', async (req, res) => {
     }
 
     try {
-        // Hash password with 10 rounds of salt
+        // Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create new user object
@@ -104,7 +61,7 @@ app.post('/users', async (req, res) => {
             name,
             age,
             email,
-            password: hashedPassword  // Store hashed password
+            password: hashedPassword  // Make sure your model has 'password' instead of 'hashedPassword'
         });
 
         // Save the user to the database
@@ -113,25 +70,28 @@ app.post('/users', async (req, res) => {
 
     } catch (err) {
         console.error('Error saving user:', err);
-        res.status(500).json({ message: 'Server Error', error: err.message || err });
+        res.status(500).json({ message: 'Server Error', error: err });
     }
 });
 
+
 // Get all users
+
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
 
-        // Remove password from the response
-        users.forEach(user => {
-            user.password = "********"; // Prevent sending password in response
-        });
-
-        res.json(users);
+        // Remove password from response
+        // users.forEach(user => {
+        //     user.password = "********";
+        // });
+        res.send(users);
     } catch (err) {
-        res.status(500).json({ message: 'Error fetching users', error: err.message || err });
+        res.status(500).send (err);
     }
-});
+})
+
+
 
 // Start server
 app.listen(port, () => {
